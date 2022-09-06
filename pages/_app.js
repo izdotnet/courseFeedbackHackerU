33ad9a187c3dca007eb49c7f5fa27693fb15e261
th3hate <mo7iballah@gmail.com>
2022-09-06 12:39:06 +0300

    Updated global css

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }) {
           @import
           url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
         </style>
+
         <title>Course Feedback</title>
         <meta
           name="viewport"
